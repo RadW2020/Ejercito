15f6f3892705ad4079aa0b00bca1750f9b726376
Redheadcore <amircas96@gmail.com>
2016-04-14 18:49:04 +0200

    Added files via upload

--- Puesto.java
+++ Puesto.java
@@ -1,15 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+package raul.s.application;
 
 /**
  *
- * @author christian
+ * @author Grupo 3
  */
+
 public class Puesto {
-    String codigo;
-    String nombreUnidad;
-    String localidad;
+
+    private String codigo;
+    private String nombre_unidad;
+    private String localidad;
+    private boolean disponible;
+
+    public Puesto(String codigo, String nombre_unidad, String localidad, boolean disponible) {
+        this.codigo = codigo;
+        this.nombre_unidad = nombre_unidad;
+        this.localidad = localidad;
+        this.disponible = disponible;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre_unidad() {
+        return nombre_unidad;
+    }
+
+    public void setNombre_unidad(String nombre_unidad) {
+        this.nombre_unidad = nombre_unidad;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+    @Override
+    public String toString () {            
+            return "Codigo: " + codigo + " Nombre de unidad: " + nombre_unidad + " Localidad: " + localidad + " Disponibilidad: " + disponible;
+    }
+  
+    
 }
